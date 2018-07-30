720da277c1c907b7df2c8403ee6d2bd009778b42
JanaJavno <perednia.jana@gmail.com>
2018-07-30 15:06:47 +0300

    Authorization with default password encoder

--- src/main/java/com/example/ItraProj/GreetingController.java
+++ src/main/java/com/example/ItraProj/GreetingController.java
@@ -16,14 +16,12 @@ public class GreetingController {
     @Autowired
     private UserRepo userRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
-                           Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model){
         Iterable<User> users = userRepo.findAll();
 
@@ -32,17 +30,13 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String post(@RequestParam String name, @RequestParam String state,
                       Map<String, Object> model){
         User user = new User(name, state);
         userRepo.save(user);
         Iterable<User> users = userRepo.findAll();
         model.put("users", users);
-        return "main";
-    }
-    @PostMapping
-    public String filter(@RequestParam String name, Map<String, Object> model){
         return "main";
     }
 }
---
+++ src/main/java/com/example/ItraProj/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.ItraProj.domain;
+
+public enum Role {
+    USER;
+}
